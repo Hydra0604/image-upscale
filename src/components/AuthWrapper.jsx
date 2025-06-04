4b5fcb6988199c8bdd2e661b2050af3eb6f2b460
Hydra0604 <ak7967010@gmail.com>
2025-06-04 18:51:22 +0530

    updated3

--- src/components/AuthWrapper.jsx
+++ src/components/AuthWrapper.jsx
@@ -1,7 +1,7 @@
 import { ClerkProvider } from '@clerk/clerk-react';
 
 const AuthWrapper = ({ children }) => {
-  const publishableKey = 'pk_test_ZGl2ZXJzZS1ib2FyLTY3LmNsZXJrLmFjY291bnRzLmRldiQ';
+  const publishableKey = import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
   return (
     <ClerkProvider publishableKey={publishableKey}>
